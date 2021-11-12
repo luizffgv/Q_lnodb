cc90b9f4a7f1511941bde58abe95cbf50e5a718d
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2021-11-12 12:31:37 -0300

    Handle $0 correctly
    Closes #1

--- q_lnodb.c
+++ q_lnodb.c
@@ -2,5 +2,6 @@
 
 void Q_lnodb(double value)
 {
-    printf("You need one $%.2f bill.\n", value);
+    printf("You need %s $%.2f bill%s.\n",
+           value ? "one" : "zero", value, value ? "" : "s");
 }
